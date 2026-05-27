1f2c7939534ed50db0f84bb4a617c7195d89d2ab
Yovick RZ <66042604+AyrXZ47@users.noreply.github.com>
2026-05-26 19:38:58 -0600

    fix(render-markdown): actualizar ruta del repositorio de mini.nvim

--- nvim/render-markdown.lua
+++ nvim/render-markdown.lua
@@ -2,7 +2,7 @@ return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
-		"echasnovski/mini.nvim",
+		"nvim-mini/mini.nvim",
 	},
 	opts = {
 		heading = {
